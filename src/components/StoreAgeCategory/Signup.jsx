51210fd324f492e3d7daee45d83511a6b0335fc9
MohdEhtesham <ehteshamidrishi@gmail.com>
2023-01-06 20:01:21 +0530

    solvedconflicts

--- src/components/StoreAgeCategory/Signup.jsx
+++ src/components/StoreAgeCategory/Signup.jsx
@@ -2,19 +2,35 @@ import { useState } from 'react';
 
 import { Button, Form, Segment } from 'semantic-ui-react'
 import Footer from '../Footer/Footer';
+import Login from './Login';
 
-export default function Signup() {
+export default function Signup({ register }) {
 
 
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-
-
     const [submitted, setSubmitted] = useState(false);
     const [error, setError] = useState(false);
+    const [dataInput, setDataInput] = useState([]);
 
 
+    const submitThis = () => {
+        const info = { name: name, email: email, password: password };
+        setDataInput([info]);
+        // localStorage.setItem('datainput', JSON.stringify([...dataInput, info]));
+        let olddata = localStorage.getItem('datainput');
+        if (olddata == null) {
+            olddata = []
+            olddata.push(info)
+            localStorage.setItem('datainput', JSON.stringify(olddata));
+        } else {
+            let oldArr = JSON.parse(olddata)
+            oldArr.push(info)
+            localStorage.setItem("datainput", JSON.stringify(oldArr))
+            console.log(oldArr, 'hhg')
+        }
+    }
     const handleName = (e) => {
         setName(e.target.value);
         setSubmitted(false);
@@ -43,7 +59,6 @@ export default function Signup() {
         }
     };
 
-
     const successMessage = () => {
         return (
             <div
@@ -52,6 +67,7 @@ export default function Signup() {
                     display: submitted ? '' : 'none',
                 }}>
                 <h1>User {name} successfully registered!!</h1>
+
             </div>
         );
     };
@@ -70,47 +86,52 @@ export default function Signup() {
     };
 
     return (
-        <div className="form">
-            <div>
-                <h1>User Registration</h1>
-            </div>
-
-
-            <div className="messages">
-                {errorMessage()}
-                {successMessage()}
-            </div>
-            <div style={{width:'600px', margin:'auto'}}>
-            <img
-              className="appLogo"
-              src="https://www.thesouledstore.com/static/img/300x157-twitter.png"
-            />
+        <div>
+            <Login />
+                <div className="messages">
+                    {errorMessage()}
+                    {successMessage()}
+                </div>
+                <div className='divspace'>
+                <div style={{ width: '600px', margin: 'auto' }}>
                 <Segment >
-                    <Form>
+                    <div className='divspace'>
+                        <h1>User Registration</h1>
+                    </div>
+                   
+                        <img
+                            className="appLogo"
+                            src="https://www.thesouledstore.com/static/img/300x157-twitter.png"
+                        />
+                        <Form onSubmit={submitThis}>
 
 
-                        <Form.Input  fluid label='Name'  onChange={handleName} className="input"
-                            value={name} type="text" />
-                        <Form.Group widths={2}>
+                            <Form.Input fluid label='Name' onChange={handleName} className="input"
+                                value={name} type="text" required/>
+                            <Form.Group widths={2}>
 
-                            <Form.Input   fluid label='Email' onChange={handleEmail} className="input"
-                                value={email} type="email" />
+                                <Form.Input fluid label='Email' onChange={handleEmail} className="input"
+                                    value={email} type="email"  required/>
 
 
-                            <Form.Input  fluid label='Password'  onChange={handlePassword} className="input"
-                                value={password} type="password" />
-                        </Form.Group>
-                        <Form.Checkbox label='I agree to the Terms and Conditions' />
-                        <Button onClick={handleSubmit} className="btn" type="submit">
-                            Submit
-                        </Button>
-                    </Form>
+                                <Form.Input fluid label='Password' onChange={handlePassword} className="input"
+                                    value={password} type="password" required/>
+                            </Form.Group>
+                            <Form.Checkbox label='I agree to the Terms and Conditions' />
+                            <Button className="btn" type="submit">
+                                Submit
+                            </Button>
+                        </Form>
+
                 </Segment>
-                
-            </div>
-            <div className='divspace'>
+                </div>
+
+
+                <div className='divspace'>
                     <Footer />
                 </div>
-        </div>
+                </div>
+            </div>
+
     );
 }
