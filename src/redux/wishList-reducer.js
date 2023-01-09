2225bc06a5f51889a950dd512533911a02167b04
Kautubh Rastogi <kautubhrastogi@Kautubhs-Mac-mini.local>
2023-01-09 20:31:54 +0530

    wishtocart

--- src/redux/wishList-reducer.js
+++ src/redux/wishList-reducer.js
@@ -6,7 +6,15 @@ const wishListSlice = createSlice({
     initialState,
     reducers:{
         addWish : (state,action) => {
-            state.data={wishListItem:[...state.data.wishListItem, action.payload]}
+            console.log(action.payload,'hjdvh')
+            if(state.data.wishListItem.length===0){
+            state.data={wishListItem:[{...action.payload}]}
+            } else{
+                let newData = state.data.wishListItem.filter((i)=>i.id==action.payload.id)
+                if(newData.length===0){
+                    state.data={wishListItem:[...state.data.wishListItem, action.payload]}
+                }
+            }
         },
         removeWishListItem : (state,action) =>{
             const newArray = state.data.wishListItem.filter((item,index)=>item.id != action.payload)
