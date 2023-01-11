ea62999c2263ac12fe5a8a6769080bb50ad9b66d
githrishikesh123 <hrishikesh.bhagat@otssolutions.com>
2023-01-11 16:17:16 +0530

    header updates

--- src/Data/TryMock.js
+++ src/Data/TryMock.js
@@ -648,6 +648,247 @@ const TryMock = {
       ],
     },
   ],
+  CHILD: [
+    {
+      category: "Shirts",
+      cardOneHead: "SWEATSHIRTS & SWEATERS",
+      data: [
+        {
+          id: 1,
+          name: "Dexter: Pattern",
+          img: "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1670313765_5048030.jpg?format=webp&w=240&dpr=1.0",
+          category: "Sweatshirts",
+          imgMove:
+            "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1638958886_9557776.jpg?format=webp&w=360&dpr=1.0",
+          price: 949,
+          offPrice: 699,
+          percentOff: "43%",
+          count: 1,
+        },
+        {
+          id: 2,
+          name: "Dexter: Pattern",
+          img: "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1664012229_7593842.jpg?format=webp&w=240&dpr=1.0",
+          category: "Sweatshirts",
+          imgMove:
+            "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1664012229_1001108.jpg?format=webp&w=360&dpr=1.0",
+          price: 949,
+          offPrice: 699,
+          percentOff: "43%",
+          count: 1,
+        },
+
+        {
+          id: 3,
+          name: "Punisher: The Man",
+          img: "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1669829374_1614988.jpg?format=webp&w=240&dpr=1.0",
+          category: " Sweatshirts",
+          imgMove:
+            "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1668864982_5546099.jpg?format=webp&w=360&dpr=1.0",
+          price: 599,
+          offPrice: 359,
+          percentOff: "34%",
+          count: 1,
+        },
+        {
+          id: 4,
+          name: "Good Vibes Only",
+          img: "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1665836999_9334759.jpg?format=webp&w=240&dpr=1.0",
+          category: "Sweatshirts",
+          imgMove:
+            "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1663589939_8277548.jpg?format=webp&w=360&dpr=1.0",
+          price: 749,
+          offPrice: 599,
+          percentOff: "43%",
+          count: 1,
+        },
+        {
+          id: 5,
+          name: "Harry Potter: Hufflepuff",
+          img: "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1665560023_1584977.jpg?format=webp&w=240&dpr=1.0",
+          imgMove:
+            "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1665560023_8162839.jpg?format=webp&w=360&dpr=1.0",
+          category: "Sweatshirts ",
+          price: 699,
+          offPrice: 399,
+          percentOff: "43%",
+          count: 1,
+        },
+        {
+          id: 6,
+          name: "Superman: Man of Steel",
+          img: "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1665832607_7807006.jpg?format=webp&w=240&dpr=1.0",
+          imgMove:
+            "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1665832629_5408793.jpg?format=webp&w=360&dpr=1.0",
+          category: " Sweatshirts",
+          price: 889,
+          offPrice: 649,
+          percentOff: "43%",
+          count: 1,
+        },
+      ],
+    },
+    {
+      category: "t-shirt",
+      cardOneHead: "HOODIES & JACKETS",
+      data: [
+        {
+          id: 1,
+          name: "Solids: Red",
+          img: "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1665837511_9999370.jpg?format=webp&w=240&dpr=1.0",
+          category: "hoodies",
+          imgMove:
+            "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1664013179_8751360.jpg?format=webp&w=360&dpr=1.0",
+          price: 849,
+          offPrice: 699,
+          percentOff: "43%",
+          count: 1,
+        },
+        {
+          id: 2,
+          name: "Cartoon Network: 90's",
+          img: "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1646998704_4613157.jpg?format=webp&w=240&dpr=1.0",
+          category: "hoodies",
+          imgMove:
+            "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1646998680_1022498.jpg?format=webp&w=360&dpr=1.0",
+          price: 1249,
+          offPrice: 1199,
+          percentOff: "43%",
+          count: 1,
+        },
+
+        {
+          id: 3,
+          name: "Popeye: The Sailor Man",
+          img: "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1671605509_5542035.jpg?format=webp&w=240&dpr=1.0",
+          category: "hoodies",
+          imgMove:
+            "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1671550303_9093660.jpg?format=webp&w=360&dpr=1.0",
+          price: 599,
+          offPrice: 359,
+          percentOff: "34%",
+          count: 1,
+        },
+        {
+          id: 4,
+          name: "Solid : Soft grey",
+          img: "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1669485239_4184959.jpg?format=webp&w=240&dpr=1.0",
+          category: "hoodies",
+          imgMove:
+            "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1669485256_4771868.jpg?format=webp&w=360&dpr=1.0",
+          price: 749,
+          offPrice: 599,
+          percentOff: "43%",
+          count: 1,
+        },
+        {
+          id: 5,
+          name: "Looney Toons: Bugs Special",
+          img: "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1670086607_7310087.jpg?format=webp&w=240&dpr=1.0",
+          // imgMove:
+          //   "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1661773633_3657738.jpg?format=webp&w=576&dpr=1.0",
+          category: "hoodies",
+          imgMove:
+            "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1670086620_5318985.jpg?format=webp&w=360&dpr=1.0",
+          price: 699,
+          offPrice: 399,
+          percentOff: "43%",
+          count: 1,
+        },
+        {
+          id: 6,
+          name: "batman : Iconic bat",
+          img: "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1669265073_5083197.jpg?format=webp&w=240&dpr=1.0",
+          category: " hoodies",
+          imgMove:
+            "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1669265073_8464292.jpg?format=webp&w=360&dpr=1.0",
+          price: 889,
+          offPrice: 649,
+          percentOff: "43%",
+          count: 1,
+        },
+      ],
+    },
+    {
+      category: "Hoodies",
+      cardOneHead: `NEW ARRIVAL COLLECTION`,
+      data: [
+        {
+          id: 1,
+          name: "That's What She Said",
+          img: "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1662612611_5410923.jpg?format=webp&w=240&dpr=1.0",
+          category: "special",
+          imgMove:
+            "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1655356632_9622383.jpg?format=webp&w=360&dpr=1.0",
+          price: 849,
+          offPrice: 699,
+          percentOff: "33%",
+          count: 1,
+        },
+        {
+          id: 2,
+          name: "The Simpsons: Haters",
+          img: "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1656045266_4908683.jpg?format=webp&w=240&dpr=1.0",
+          category: "special",
+          imgMove:
+            "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1656045266_2012521.jpg?format=webp&w=360&dpr=1.0",
+          price: 749,
+          offPrice: 679,
+          percentOff: "23%",
+          count: 1,
+        },
+
+        {
+          id: 3,
+          name: "The Simpsons: Flexing",
+          img: "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1654844464_2327811.jpg?format=webp&w=240&dpr=1.0",
+          category: " special",
+          imgMove:
+            "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1654080797_2796751.jpg?format=webp&w=360&dpr=1.0",
+          price: 599,
+          offPrice: 359,
+          percentOff: "34%",
+          count: 1,
+        },
+        {
+          id: 4,
+          name: "Captain America : Shield",
+          img: "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1655571622_6747331.jpg?format=webp&w=240&dpr=1.0",
+          category: "special",
+          imgMove:
+            "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1643364456_7691750.jpg?format=webp&w=360&dpr=1.0",
+          price: 749,
+          offPrice: 599,
+          percentOff: "43%",
+          count: 1,
+        },
+        {
+          id: 5,
+          name: "Batman : Gotham's Knight",
+          img: "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1666162151_8762188.jpg?format=webp&w=240&dpr=1.0",
+          imgMove:
+            "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1656305043_4989429.jpg?format=webp&w=360&dpr=1.0",
+          category: "special",
+          price: 699,
+          offPrice: 399,
+          percentOff: "43%",
+          count: 1,
+        },
+        {
+          id: 6,
+          name: "Courage face",
+          img: "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1652179428_4908262.jpg?format=webp&w=240&dpr=1.0",
+          category: " special",
+          imgMove:
+            "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1645689551_4126373.jpg?format=webp&w=360&dpr=1.0",
+          price: 889,
+          offPrice: 649,
+          percentOff: "43%",
+          count: 1,
+        },
+      ],
+    },
+  ],
 };
 
 export default TryMock;
